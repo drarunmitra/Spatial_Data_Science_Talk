ab01371a5b334fbdb301715518b1c392de8d8543
Arun Mitra <76924876+drarunmitra@users.noreply.github.com>
2023-11-03 14:43:01 +0530

    First Commit

---
+++ git_setup.R
@@ -0,0 +1,9 @@
+# Git Set up
+
+library(usethis)
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+# 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 #This is for the slides
+
+# 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  # This must be for someother one, mostly for the NWHDA
+usethis::edit_r_environ()
