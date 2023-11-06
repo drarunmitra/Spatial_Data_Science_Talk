33d8f4a5405b20fb7644f5daa7241a06908f4f83
drarunmitra <dr.arunmitra@gmail.com>
2023-11-06 15:57:24 +0530

    Add existing project files to Git

--- git_setup.R
+++ git_setup.R
@@ -3,7 +3,14 @@
 library(usethis)
 usethis::create_github_token()
 gitcreds::gitcreds_set()
-# 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 #This is for the slides
+# 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 #This is for the slides
 
 # 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  # This must be for someother one, mostly for the NWHDA
 usethis::edit_r_environ()
+
+
+use_git_config(
+  scope = "project",
+  user.name = "drarunmitra",
+  user.email = "dr.arunmitra@gmail.com"
+)
