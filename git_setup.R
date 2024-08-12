57eda1db6d159a4debc5652b358995cc65ac737f
Arun Mitra <76924876+drarunmitra@users.noreply.github.com>
2024-08-12 14:48:55 +0530

    Update git_setup.R

--- git_setup.R
+++ git_setup.R
@@ -1,16 +1,3 @@
 # Git Set up
 
-library(usethis)
-usethis::create_github_token()
-gitcreds::gitcreds_set()
-# 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 #This is for the slides
 
-# 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  # This must be for someother one, mostly for the NWHDA
-usethis::edit_r_environ()
-
-
-use_git_config(
-  scope = "project",
-  user.name = "drarunmitra",
-  user.email = "dr.arunmitra@gmail.com"
-)
